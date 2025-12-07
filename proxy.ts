c48cc9967ef985eadab23a137a0cfd94e12ae9d6
Guillom <guillaume.vervialle@gmail.com>
2025-12-07 14:03:57 +0100

    fix: remove secretKey from middleware config

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,9 @@
-process.env.CLERK_ENCRYPTION_KEY = 'UrIV/iv5wAjLxEqOwAS5CDxDizh9a3IiKV0v6kwc/wE='
+
 
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const CLERK_PUBLISHABLE_KEY = 'pk_test_Y2hhcm1pbmctcHVwLTc4LmNsZXJrLmFjY291bnRzLmRldiQ' // ← Mettre ta clé ici
-const CLERK_SECRET_KEY = 'sk_test_z6Sr4WGwIe2qnSo5LpQwiqXDBldwqd88sCUoWKk8ZD'
+
 const isPublicRoute = createRouteMatcher([
   '/',
   '/app(.*)',
@@ -20,7 +20,6 @@ export default clerkMiddleware(
   },
   {
     publishableKey: CLERK_PUBLISHABLE_KEY,
-    secretKey: CLERK_SECRET_KEY,
   }
 )
 
